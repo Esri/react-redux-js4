c628f55a0cfd2ded2875fc71c706d6d0f2da5f3a
Michael Van den Bergh <mvandenbergh@esri.com>
2017-07-04 14:40:03 +0200

    Add Calcite

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -5,7 +5,7 @@ import { signOut } from '../reducers/identity/actions'
 
 const Info = ({ username, interacting, scale, zoom, items, _signOut_ }) =>
     <div className="list-items">
-        <p>user: {username} <button onClick={_signOut_()}>x</button></p>
+        <p>user: {username} <button className="btn btn-small btn-clear" onClick={_signOut_()}>Sign Out</button></p>
         <p>interacting: {String(interacting)}</p>
         <p>scale: {(scale || 0).toFixed(2)}</p>
         <p>zoom: {(zoom || 0).toFixed(2)}</p>
