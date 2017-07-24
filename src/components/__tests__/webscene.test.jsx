6a38cf36b1e4153a8958ac481fd2ae1c597724fc
Michael Van den Bergh <mvandenbergh@esri.com>
2017-07-24 08:43:57 +0200

    Webscene component test

---
+++ src/components/__tests__/webscene.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { WebSceneView } from '../webscene';
+
+function setup() {
+  const props = {
+    init: jest.fn(),
+  };
+  const wrapper = mount(<WebSceneView {...props} />);
+
+  return { props, wrapper };
+}
+
+describe('components', () => {
+  describe('<WebSceneView />', () => {
+    it('should render self and call init()', () => {
+      const { props, wrapper } = setup();
+      expect(wrapper.find('div').hasClass('websceneview')).toBe(true);
+      expect(props.init).toHaveBeenCalled();
+      expect(props.init).toHaveBeenCalledWith(wrapper.find('div').node);
+    });
+  });
+});
--- src/components/webscene.jsx
+++ src/components/webscene.jsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux';
 
 import { initSceneView } from '../reducer/webscene/actions';
 
-class WebSceneView extends React.Component {
+export class WebSceneView extends React.Component {
   componentDidMount() {
     this.props.init(this.sceneView);
   }
