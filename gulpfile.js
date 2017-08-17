d34d166f384e4ca1e7c876d32478fd3c3637a005
Michael Van den Bergh <mvandenbergh@esri.com>
2017-08-17 14:18:43 +0200

    Add Webpack in order to support Common JS modules (together with Babel and Dojo / AMD modules)

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ const babel = require('gulp-babel');
 const connect = require('gulp-connect');
 const eslint = require('gulp-eslint');
 
+const gutil = require('gulp-util');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.babel');
+const WebpackDevServer = require('webpack-dev-server');
+
 gulp.task('react', () => gulp.src([
   'src/*.jsx', 'src/**/*.jsx', 'src/**/**/*.jsx',
   'src/*.js', 'src/**/*.js', 'src/**/**/*.js',
@@ -35,5 +40,60 @@ gulp.task('lint', () => gulp.src([
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError()));
+
+const esri = {
+  'esri/config': {
+    amd: 'esri/config',
+  },
+  'esri/WebScene': {
+    amd: 'esri/WebScene',
+  },
+  'esri/identity/IdentityManager': {
+    amd: 'esri/identity/IdentityManager',
+  },
+  'esri/identity/OAuthInfo': {
+    amd: 'esri/identity/OAuthInfo',
+  },
+  'esri/portal/Portal': {
+    amd: 'esri/portal/Portal',
+  },
+  'esri/views/SceneView': {
+    amd: 'esri/views/SceneView',
+  },
+};
+
+gulp.task('webpack', ['react'], (callback) => {
+  const myConfig = Object.create(webpackConfig);
+  myConfig.externals = esri;
+
+  // run webpack
+  webpack(myConfig, (err, stats) => {
+    if (err) throw new gutil.PluginError('webpack', err);
+    gutil.log('[webpack]', stats.toString({
+      colors: true,
+      progress: true,
+    }));
+    callback();
+  });
+});
+
+gulp.task('server', ['webpack'], () => {
+  // modify some webpack config options
+  const myConfig = Object.create(webpackConfig);
+  myConfig.externals = esri;
+
+  // Start a webpack-dev-server
+  new WebpackDevServer(webpack(myConfig), {
+    publicPath: '/' + myConfig.output.publicPath,
+    stats: {
+      colors: true,
+    },
+    hot: true,
+  }).listen(8080, 'localhost', (err) => {
+    if (err) throw new gutil.PluginError('webpack-dev-server', err);
+    gutil.log('[webpack-dev-server]', 'http://localhost:8080/webpack-dev-server/index.html');
+  });
+});
+
 
 gulp.task('default', ['react', 'webserver', 'watch']);
---
+++ webpack.config.babel.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    preload: './dist/main.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '../dist/',
+    filename: 'bundle.js',
+    chunkFilename: '[id].bundle.js',
+  },
+};
