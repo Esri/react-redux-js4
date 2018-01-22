fbcac10643c17754dcd392400a6a7d349863feca
Michael Van den Bergh <19687652+mvdbster@users.noreply.github.com>
2018-01-22 15:08:29 +0100

    remove app id

--- src/constants/app-constants.js
+++ src/constants/app-constants.js
@@ -14,7 +14,7 @@
  *
  */
 
-export const APP_ID = 'msDiyjoc2RL55UZU';
+export const APP_ID = '<insert id here>';
 export const APP_PORTAL_URL = 'https://www.arcgis.com';
 
 export const DEFAULT_SCENE_ID = '63a16e0c9f364d0fab9d55f40bf71771';
